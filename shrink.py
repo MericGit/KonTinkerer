2a94a4891d0438366e59fe89e3cac0a06434e87d
MericGit <lawrencezhang04@gmail.com>
2021-10-03 21:20:21 -0500

    Update shrink.py

--- shrink.py
+++ shrink.py
@@ -1,10 +1,10 @@
 import ffmpeg
 import os
-rootdir = 'C:\\Users\\dongd\\Downloads\\testffmpeg'
+rootdir = "C:\\Users\\dongd\\Downloads\\testffmpeg\\input\\"
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         if file.endswith("wav"):
             print(os.path.join(subdir, file))
             print('ran')
-            os.system("ffmpeg -i" + file + "-acodec wav -af aresample=osf=s16:dither_method=rectangular" + "output.wav")
+            os.system("C:\\ffmpeg\\bin\\ffmpeg.exe -hide_banner -loglevel error -i " + os.path.join(subdir, file) + " -acodec pcm_s16le -af aresample=osf=s16:dither_method=rectangular -nostdin " + "C:\\Users\\dongd\\Downloads\\testffmpeg\\test2\\" + file )
