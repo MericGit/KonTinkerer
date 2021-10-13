53480626fafae6aabfce2809571e88c6e6c6f22e
MericGit <lawrencezhang04@gmail.com>
2021-10-12 19:19:45 -0500

    Update shrink.py

--- shrink.py
+++ shrink.py
@@ -2,8 +2,12 @@ import ffmpeg
 import os
 import subprocess
 import time
-rootdir = "C:\\Users\\dongd\\Downloads\\testffmpeg\\Koto-2\\Collected-Samples\\testdir"
-workingdir = 
+rootdir = "C:\\Users\\dongd\\Downloads\\testffmpeg\\Collected-Samples\\Samples"
+workingdir = "C:\\Users\\dongd\\Downloads\\testffmpeg\\Collected-Samples\\Samples2"
+
+wavtoncw = False
+
+
 
 count = 0
 current = 0
@@ -22,26 +26,25 @@ for subdir, dirs, files in os.walk(rootdir):
         if file.endswith("wav"):
             current+=1
             print('Working... (' + str(current) + " / " + str(count) + ")")
-            subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'error', '-i', os.path.join(subdir, file), '-acodec', 'pcm_s16le', '-af', 'aresample=osf=s16:dither_method=rectangular', '-nostdin', workingdir])
+            subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'error', '-i', os.path.join(subdir, file), '-acodec', 'pcm_s16le', '-af', 'aresample=osf=s16:dither_method=rectangular', '-nostdin', os.path.join(workingdir,file)    ])
 
 print("Stage 1: Wav 24bit --> Wav 16bit Completed")
 print("Remember! Old directory and files are kept as a backup. Please delete after program completes")
 print("Beginning Stage 2: Wav16bit --> ncw")
 time.sleep(3)
+if wavtoncw is True:
+    current = 0
+    subprocess.call(['conNCW04.exe', '-w2n',workingdir])
+    print("Stage 2: Wav16bit --> ncw completed")
+    print("Beginning Stage 3: Wav16bit deletion")
+    time.sleep(2)
+    for subdir, dirs, files in os.walk(workingdir):
+        for file in files:
+            if file.endswith("wav"):
+                current+=1
+                print('Working... (' + str(current) + " / " + str(count) + ")")
+                os.remove(os.path.join(subdir, file))
 
-current = 0
-subprocess.call(['C:\\Users\\dongd\\Documents\\Lawrence\\xCON\\conNCW04.exe', '-w2n',workingdir])
 
-print("Stage 2: Wav16bit --> ncw completed")
-print("Beginning Stage 3: Wav16bit deletion")
-time.sleep(2)
-for subdir, dirs, files in os.walk(workingdir):
-    for file in files:
-        if file.endswith("wav"):
-            current+=1
-            print('Working... (' + str(current) + " / " + str(count) + ")")
-            os.remove(os.path.join(subdir, file))
-
-
-print("Stage 3 Wav16bit deletion completed!")
+    print("Stage 3 Wav16bit deletion completed!")
 print("Shrinking has finished. Original samples are kept intact, remember to delete")
