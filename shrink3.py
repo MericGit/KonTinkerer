23f1c72ec9f38f93f04c96e69bde832ab88490b7
MericGit <lawrencezhang04@gmail.com>
2021-12-13 23:07:51 -0600

    Update shrink3.py

--- shrink3.py
+++ shrink3.py
@@ -16,23 +16,22 @@ ffmpeg = os.path.dirname(os.path.realpath(__file__)) + "\Resources\\ffmpegReduce
 
 #Set this to the folder containing a folder that contains your samples. No ncw should be directly in this dir, only a folder containing them.
 #Designed this way to easily accomodate direct ncx unpacking into this dir. 
-rootdir = "C:\\Users\\dongd\\Downloads\\JB\\Samples\\str"
+rootdir = "C:\\Users\\dongd\\Downloads\\JB\\Samples"
 print("Script start: " + str(startTime))
-micTarget = ["AB","SURROUND"]   #Mic positions you want to delete. Case sensitive, type exactly as is in the filename.
+micTarget = ["SRND"]   #Mic positions you want to delete. Case sensitive, type exactly as is in the filename.
 whitelist = ["null"]  #Samples containing these words are ignored and not processed.
 doShrink = False
-pause = False
 
 
 def scan():
     global count
     count = 0
     print("Beginning Stage A: Pre-Startup")
-    print("Scanning Sor samples...")
+    print("Scanning For samples...")
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
             if file.endswith("ncw"):
-                count+=1
+                count += 1
             else:
                 print("NON ncw file: " + os.path.join(subdir,file) + " found")
                 #os.remove(os.path.join(subdir,file))
@@ -51,8 +50,8 @@ def osWalk():
                     current += 1
                     if micDelete(subdir, file) is False:
                         #print('(' + str(current) + ' / ' + str(count) + ') ', end='')
-                        print('(' + str(current) + ' / ' + str(count) + ')',end='')
                         if doShrink is True:
+                            print('(' + str(current) + ' / ' + str(count) + ')',end='')
                             shrink(subdir,file)
                 else:
                     print("NON ncw file located in directory - Ignoring file: " + os.path.join(subdir,file))
@@ -76,39 +75,17 @@ def shrink(subdir,file):
         subprocess.call([xCon,os.path.join(subdir,file.replace('ncw','wav',1))])
         print("\033[A\033[A")
         os.remove(os.path.join(subdir,file.replace('ncw','wav',1)))
-
 def micDelete(subdir, file):
     global micTarget
     if not any(ignore in file for ignore in whitelist) and any(mic in file for mic in micTarget):
-    #if any(mic in file for mic in micTarget) and not any(ignore in file for ignore in whitelist):
         copyfile(os.path.dirname(os.path.realpath(__file__)) + "\Resources\\micReplace.ncw", os.path.join(subdir,file))
-        #print(file)
         return True
     else:
         return False
-
-def checkWhitelist(subdir, file):
-    if any(target in file for target in whitelist):
-        return True
-    else:
-        return
-        False
-
-def checkMicTarget(subdir, file):
-    if any(target in file for arget in micTarget):
-        return True
-    else:
-        return False
-
-
-def micMerge(mixTargets,replaceTarget):
-    print("later")
-
-
 
 
 start = time.time()
 scan()
-osWalk()
+#osWalk()
 end = time.time()
 print("Shrink finished in: " + str(datetime.timedelta(seconds=int(end - start))))
